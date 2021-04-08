7579c45d325e60a5ea2d1598e81d5a5479afb4fb
William Welling <wwelling@library.tamu.edu>
2021-04-08 15:27:06 -0500

    add viewport

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,13 +9,14 @@ if (urlParams.has('manifest')) {
 
 const viewer = OpenSeadragon({
   id: "viewer",
-  preserveViewport: true,
-  visibilityRatio: 1,
-  minZoomLevel: 1,
-  defaultZoomLevel: 1,
-  smoothTileEdgesMinZoom: 1,
-  animationTime: 1,
-  opacity: 1,
+  showNavigator:  true,
+  // preserveViewport: true,
+  // visibilityRatio: 1,
+  // minZoomLevel: 1,
+  // defaultZoomLevel: 1,
+  // smoothTileEdgesMinZoom: 1,
+  // animationTime: 1,
+  // opacity: 1,
   prefixUrl: "/openseadragon/images/",
   tileSources: [
     manifest
